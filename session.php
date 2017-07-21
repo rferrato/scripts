401dbf7e126c5885deb647105a44537273d87733
Rafael Ferrato <rafael@wed.biz>
2017-07-21 16:08:36 -0300

    Ultima alteração ...

--- session.php
+++ session.php
@@ -5,12 +5,12 @@ if(isset($_SESSION['teste']))
 {
 	$tst = $_SESSION['teste'];
 	unset($_SESSION['teste']);
-	echo $tst.PHP_EOL; // teste 123
+	echo $tst.PHP_EOL; // teste 123 456
 	print_r($_SESSION);	
 }
 else
 {
-	$_SESSION['teste'] = '123';
+	$_SESSION['teste'] = '123 456';
 }
 
 session_write_close();
